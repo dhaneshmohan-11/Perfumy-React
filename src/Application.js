1403d526480e2a870572e05feb64b60afd4d63cc
dhaneshmohan-11 <dhaneshmohan-11>
2024-03-08 22:11:46 +0530

    Update project

---
+++ src/Application.js
@@ -0,0 +1,21 @@
+import Navbar from './components/navbar'
+import Search from './components/search'
+import Product from './components/product'
+import AboutUs from './components/AboutUs'
+import Footer from './components/Footer'
+
+
+
+
+function Application() {
+    return (<>
+        <Navbar></Navbar>
+        <Search></Search>
+        <Product></Product>
+        <AboutUs></AboutUs>
+        <Footer></Footer>
+    </>)
+
+}
+
+export default Application
